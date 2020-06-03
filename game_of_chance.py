342e67036b908c67000e20872ae9dd01ce81293a
Mista Ginger <c.sullivan17mg@gmail.com>
2020-06-02 21:19:36 -0500

    game_of_chance.py

--- game_of_chance.py
+++ game_of_chance.py
@@ -3,39 +3,43 @@ import time
 
 money = 100
 invalid_guess = "That was not a valid guess."
-game_lost = "Sorry, you lost."
+game_lost = "Sorry, you lost the game and what money ou wagered."
 game_won = "Congratulations! You won $%s!"
 
-#Write your game of chance functions here
+#Games of Chance:
+#Coin Flipping (Guessing either heads or tails)
 def coin_flip(guess, bet):
     flip = random.randint(1, 2)
     if flip == 1 and (guess == "HEADS" or guess == "TAILS"):
-        return game_won % bet
+        print(game_won % bet)
         money += bet
+        return wallet
     elif guess != "HEADS" and guess != "TAILS":
         return invalid_guess
     else:
-        return game_lost
+        print(game_lost)
         money -= bet
 
+#Cho Han (Guessing the outcome of a 2d6 roll)
 def cho_han(guess, bet):
     roll = random.randint(1, 6) + random.randint(1, 6)
     try:
         guess = int(guess)
     except:
-        return invalid_guess
+        print(invalid_guess)
     else:
         guess = int(guess)
     time.sleep(1)
     if guess == roll:
-        return game_won % bet
+        print(game_won % bet)
         money += bet
     elif guess > 12 or guess < 2:
-        return invalid_guess
+        print(invalid_guess)
     else:
-        return game_lost + " The roll was %s." % roll
+        print(game_lost + " The roll was %s." % roll)
         money -= bet
 
+#Card Drawing (Hoping for a better draw than the dealer)
 def card_draw(bet):
     draw_to_beat = random.randint(1, 13)
     player_draw = random.randint(1, 13)
@@ -47,15 +51,33 @@ def card_draw(bet):
     print("You drew a %s." % card_values[player_draw])
     time.sleep(1)
     if player_draw > draw_to_beat:
-        return game_won % bet
+        print(game_won % bet)
         money += bet
     elif player_draw < draw_to_beat:
-        return game_lost % bet
+        print(game_lost % bet)
         money -= bet
     else:
-        return "It's a tie!"
+        print("It's a tie!")
 
-#Call your game of chance functions here
-#print(coin_flip(input("Heads or Tails? ").upper(), str(input("And how much would you like to wager? "))))
-#print(cho_han(input("What are the dice going to roll? "), str(input("And how much would you like to wager? "))))
-#print(card_draw(str(input("And how much would you like to wager? "))))
+#Game Hall:
+#Navigating the different games
+def game_hall():
+    choice = ""
+    while choice != "X":
+        print("Welcome to Cole's Game Hall!")
+        choice = input("What would you like to play?\n  [F]: Coin Flip  [H]: Cho Han  [D]: Card Draw [X]: Quit\n")
+        if choice == "F":
+            print("You have chosen Coin Flip!")
+            coin_flip((input("Head or Tails? ")).upper(), (input("How much would you like to wager? ")))
+        elif choice == "H":
+            print("You have chosen Cho Han!")
+            cho_han((input("What will the dice roll? ")).upper(), (input("How much would you like to wager? ")))
+        elif choice == "D":
+            print("You have chosen Card Draw!")
+            card_draw(input("How much would you like to wager? "))
+        elif choice == "X":
+            return "Thanks for playing! You finished with %s." % (money)
+            break
+
+
+game_hall()
